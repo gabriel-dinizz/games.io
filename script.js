c5d0b4fce0c125f74c7f2021701b7c3c22689a60
Gabriel Diniz Cremel <dinizcremelgabriel@gmail.com>
2024-08-24 15:24:52 -0300

    commit

--- script.js
+++ script.js
@@ -3,6 +3,20 @@
 let randomNumber = Math.trunc(Number(Math.random() * 20 + 1));
 document.querySelector(".number").textContent = randomNumber;
 
+let score = document.querySelector(".score").value;
+score = 20;
+
+let highScore = function () {
+  if (highScore < score) {
+    highScore = highScore;
+  }
+};
+
+document.querySelector(".highScore").textContent =
+  highScore =
+  highScore +=
+    score;
+
 document.querySelector(".check").addEventListener("click", function () {
   let userGuess = Number(document.querySelector(".guess").value);
 
@@ -10,8 +24,12 @@ document.querySelector(".check").addEventListener("click", function () {
     console.log("Correct Number");
     document.querySelector(".message").textContent = "Correct number";
   } else if (userGuess < randomNumber) {
+    score--;
+    document.querySelector(".score").textContent = score;
     document.querySelector(".message").textContent = "Number is higher";
   } else if (userGuess > randomNumber) {
+    score--;
+    document.querySelector(".score").textContent = score;
     document.querySelector(".message").textContent = "Number is lower";
   }
   console.log(typeof userGuess);
