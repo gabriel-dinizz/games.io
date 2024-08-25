49f33ba07897564f80bffd9a9e8f8bf826bfe926
Gabriel Diniz Cremel <dinizcremelgabriel@gmail.com>
2024-08-25 14:41:05 -0300

    again, highscore, style

--- luckyNumber/script.js
+++ luckyNumber/script.js
@@ -6,16 +6,8 @@ document.querySelector(".number").textContent = randomNumber;
 let score = document.querySelector(".score").value;
 score = 20;
 
-let highScore = function () {
-  if (highScore < score) {
-    highScore = highScore;
-  }
-};
-
-document.querySelector(".highScore").textContent =
-  highScore =
-  highScore +=
-    score;
+let highScore = 0;
+const highScoreElement = document.querySelector(".highScore");
 
 document.querySelector(".check").addEventListener("click", function () {
   let userGuess = Number(document.querySelector(".guess").value);
@@ -23,14 +15,26 @@ document.querySelector(".check").addEventListener("click", function () {
   if (userGuess === randomNumber) {
     console.log("Correct Number");
     document.querySelector(".message").textContent = "Correct number";
+    if (score > highScore) {
+      highScore = score;
+      highScoreElement.textContent = highScore;
+    }
   } else if (userGuess < randomNumber) {
     score--;
     document.querySelector(".score").textContent = score;
     document.querySelector(".message").textContent = "Number is higher";
   } else if (userGuess > randomNumber) {
     score--;
-    document.querySelector(".score").textContent = score;
     document.querySelector(".message").textContent = "Number is lower";
   }
   console.log(typeof userGuess);
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".score").textContent = score = 20;
+  document.querySelector(".message").textContent = "Start Guessing";
+  randomNumber = document.querySelector(".number").textContent = Math.trunc(
+    Number(Math.random() * 20 + 1)
+  );
+  document.querySelector(".guess").value = " ";
 });
