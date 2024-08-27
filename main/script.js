a20050749708b43dfa313d5f1b1fc4122edb8f27
Gabriel Diniz Cremel <dinizcremelgabriel@gmail.com>
2024-08-27 08:47:31 -0300

    commit

--- main/script.js
+++ main/script.js
@@ -1,5 +1,35 @@
-const modal = document.querySelector(".modal");
+"use strict";
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(showModal[i].addEventListener("click", showModal));
-}
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+const button = document.querySelector(".btn");
+// close modal function
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// close the modal when the close button and overlay is clicked
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+button.addEventListener("click", function () {
+  location.href = "/Users/gabrieldiniz/Desktop/games.io/luckyNumber/index.html";
+});
+
+// close modal when the Esc key is pressed
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// open modal function
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+// open modal event
+openModalBtn.addEventListener("click", openModal);
